4487cec1eb88e77edcdacff2ebb1238944448c06
Gustavo Bernardo <gustavobernardobas@gmail.com>
2021-02-06 13:33:32 -0300

    final

--- main.js
+++ main.js
@@ -70,3 +70,146 @@ function validarcep() {
         alert("CEP Inválido!")
     }
 }
+
+function validarCampos(){
+    var Nome = formuser.Nome.value;
+    if (Nome == ""){
+        alert('Preencha o campo Nome do funcionário.');
+        formuser.Nome.focus();
+        return false;
+    }
+    var cpf = formuser.cpf.value;
+    if (cpf == ""){
+        alert('Preencha o campo CPF.');
+        formuser.cpf.focus();
+        return false;
+    }
+
+    var rg = formuser.rg.value;
+    if (rg == ""){
+        alert('Preencha o campo RG.');
+        formuser.rg.focus();
+        return false;
+    }
+
+    
+    var data = formuser.data.value;
+    if (data == ""){
+        alert('Preencha o campo Data de Nascimento.');
+        formuser.data.focus();
+        return false;
+    }
+    
+    var cnh = formuser.cnh.value;
+    if (cnh == ""){
+        alert('Preencha o campo do tipo de CNH.');
+        formuser.cnh.focus();
+        return false;
+    }
+    
+    var ecivil = formuser.ecivil.value;
+    if (ecivil == ""){
+        alert('Preencha o campo do estado civil.');
+        formuser.ecivil.focus();
+        return false;
+    }
+    
+    var cep = formuser.cep.value;
+    if (cep == ""){
+        alert('Preencha o campo CEP.');
+        formuser.cep.focus();
+        return false;
+    }
+
+    var rua = formuser.rua.value;
+    if (rua == ""){
+        alert('Preencha o campo Rua.');
+        formuser.rua.focus();
+        return false;
+    }
+
+    var bairro = formuser.bairro.value;
+    if (bairro == ""){
+        alert('Preencha o campo Bairro.');
+        formuser.bairro.focus();
+        return false;
+    }
+
+    var cidade = formuser.cidade.value;
+    if (cidade == ""){
+        alert('Preencha o campo Cidade.');
+        formuser.cidade.focus();
+        return false;
+    }
+
+    var estado = formuser.estado.value;
+    if (estado == ""){
+        alert('Preencha o campo Estado');
+        formuser.estado.focus();
+        return false;
+    }
+
+    var num = formuser.num.value;
+    if (num == ""){
+        alert('Preencha o campo Numero da moradia.');
+        formuser.num.focus();
+        return false;
+    }
+
+    var complemento = formuser.complemento.value;
+    if (complemento == ""){
+        alert('Preencha o campo de complemento.');
+        formuser.complemento.focus();
+        return false;
+    }
+
+    const fields = {
+        "Nome": formuser.Nome,
+        "CPF" : formuser.cpf.value,
+        "RG": formuser.rg.value,
+        "data": formuser.data.value,
+        "estado-civil": formuser.ecivil.value,
+        "CEP": formuser.rg.value,
+        "rua": formuser.rua.value,
+        "bairro": formuser.bairro.value,
+        "cidade": formuser.cidade.value,
+        "estado": formuser.estado.value,
+        "numero": formuser.numero.value,
+        "complemento": formuser.complemento[1].value,
+
+    }
+  
+
+const objetojson = JSON.stringify(fields)
+
+const xhr = new XMLHttpRequest();
+
+xhr.open('POST','https://beginner-api.herokuapp.com/save')
+
+
+
+}
+
+function getDadosEnderecoCEP(cep) {
+
+    let xhr = new XMLHttpRequest()
+
+    let url = 'https://viacep.com.br/ws/' + cep + '/json/unicode/'
+
+    xhr.open('GET', url, true)
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+             let dadosJSONText = xhr.responseText
+                let dadosJSONObj = JSON.parse(dadosJSONText)
+
+                document.getElementById('rua').value = dadosJSONObj.logradouro
+                document.getElementById('bairro').value = dadosJSONObj.bairro
+                document.getElementById('cidade').value = dadosJSONObj.localidade
+                document.getElementById('estado').value = dadosJSONObj.uf
+            }
+        }
+    }
+    xhr.send();
+}
